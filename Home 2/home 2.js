09bad29fa633cc28edccada46b5f41d7e587c576
Kisilenko Alexey <namelexa@gmail.com>
2019-10-22 18:20:48 +0300

    fixed func countWater

--- Home 2/home 2.js
+++ Home 2/home 2.js
@@ -1,34 +1,30 @@
-let array = [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0];
-let oldArray = [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0];
-
-function countWater(array, oldArray) {
+function countWater(array) {
+    const newArray = [...array];
     let maxVal = getMaxOfArray(array);
     let maxIndex = array.indexOf(maxVal);
-    let i = 0;
-    array.reduce((v, currentValue, index, arr) => {
-        let previousValue = array[index - 1]
+    newArray.reduce((v, currentValue, index, arr) => {
+        let previousValue = newArray[index - 1]
         if (maxIndex > index) {
             if (previousValue > currentValue) {
-                array[index] = previousValue;
+                newArray[index] = previousValue;
             }
 
         }
     });
-    array.reduceRight((v, currentValue, index, arr) => {
-        let previousValue = array[index + 1]
+    newArray.reduceRight((v, currentValue, index, arr) => {
+        let previousValue = newArray[index + 1]
         if (maxIndex < index) {
-        	if (previousValue > currentValue) {
-                array[index] = previousValue;
+            if (previousValue > currentValue) {
+                newArray[index] = previousValue;
             }
         }
     });
-    let newArrayCount = array.reduce((a, b) => a + b);
-    let oldArrayCount = oldArray.reduce((a, b) => a + b);
+    let newArrayCount = newArray.reduce((a, b) => a + b);
+    let oldArrayCount = array.reduce((a, b) => a + b);
 
     let result = newArrayCount - oldArrayCount;
     console.log(result)
 }
-
 function getMaxOfArray(array) {
     return Math.max.apply(null, array);
 }
